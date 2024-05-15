e25368f2eb46277084a0551ed26eb1c9f300213e
ESLAM <102685868+islam-abdel-halem@users.noreply.github.com>
2024-05-15 22:07:38 +0300

    add Questions and Exam page

---
+++ app/src/main/java/com/example/onlineexams/Exam.java
@@ -0,0 +1,163 @@
+package com.example.onlineexams;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
+public class Exam extends AppCompatActivity {
+
+    private Question[] data;
+    private String quizID;
+    private String uid;
+    private int oldToatalPoints = 0;
+    private int oldToatalQustions = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam);
+
+        quizID = getIntent().getStringExtra("Quiz ID");
+        ListView listview = findViewById(R.id.listview);
+        Button submit = findViewById(R.id.submit);
+        TextView title = findViewById(R.id.title);
+
+        uid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child("Quizzes").hasChild(quizID)) {
+                    DataSnapshot ref = snapshot.child("Quizzes").child(quizID);
+                    title.setText(Objects.requireNonNull(ref.child("Title").getValue()).toString());
+                    int num = Integer.parseInt(Objects.requireNonNull(ref.child("Total Questions").getValue()).toString());
+                    data = new Question[num];
+                    for (int i = 0; i < num; i++) {
+                        DataSnapshot qRef = ref.child("Questions").child(String.valueOf(i));
+                        Question question = new Question();
+                        question.setQuestion(Objects.requireNonNull(qRef.child("Question").getValue()).toString());
+                        question.setOption1(Objects.requireNonNull(qRef.child("Option 1").getValue()).toString());
+                        question.setOption2(Objects.requireNonNull(qRef.child("Option 2").getValue()).toString());
+                        question.setOption3(Objects.requireNonNull(qRef.child("Option 3").getValue()).toString());
+                        question.setOption4(Objects.requireNonNull(qRef.child("Option 4").getValue()).toString());
+                        int ans = Integer.parseInt(Objects.requireNonNull(qRef.child("Ans").getValue()).toString());
+                        question.setCorrectAnswer(ans);
+                        data[i] = question;
+                    }
+                    ListAdapter listAdapter = new ListAdapter(data);
+                    listview.setAdapter(listAdapter);
+                    DataSnapshot ref2 = snapshot.child("Users").child(uid);
+                    if (ref2.hasChild("Total Points")) {
+                        oldToatalPoints = Integer.parseInt(Objects.requireNonNull(ref2.child("Tolal Point").getValue()).toString());
+                    }
+                    if (ref2.hasChild("Total Questions")) {
+                        oldToatalQustions = Integer.parseInt(Objects.requireNonNull(ref2.child("Tolal Questions").getValue()).toString());
+                    }
+                } else {
+                    finish();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(Exam.this, "can't connect", Toast.LENGTH_SHORT).show();
+            }
+        };
+        database.addValueEventListener(listener);
+
+    }
+
+    public class ListAdapter extends BaseAdapter {
+        Question[] arr;
+
+        ListAdapter(Question[] arr2) {
+            arr = arr2;
+        }
+
+        @Override
+        public int getCount() {
+            return arr.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return arr[i];
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View View, ViewGroup viewGroup) {
+
+            LayoutInflater inflater = getLayoutInflater();
+            @SuppressLint({"ViewHolder", "InflateParams"}) View v = inflater.inflate(R.layout.question, null);
+
+            TextView question = v.findViewById(R.id.question);
+            RadioButton option1 = v.findViewById(R.id.option1);
+            RadioButton option2 = v.findViewById(R.id.option2);
+            RadioButton option3 = v.findViewById(R.id.option3);
+            RadioButton option4 = v.findViewById(R.id.option4);
+
+            question.setText(data[i].getQuestion());
+            option1.setText(data[i].getOption1());
+            option2.setText(data[i].getOption2());
+            option3.setText(data[i].getOption3());
+            option4.setText(data[i].getOption4());
+
+            option1.setOnCheckedChangeListener((compoundButton, b) -> {
+                if (b) data[i].setSelectedAnswer(1);
+            });
+            option2.setOnCheckedChangeListener((compoundButton, b) -> {
+                if (b) data[i].setSelectedAnswer(2);
+            });
+            option3.setOnCheckedChangeListener((compoundButton, b) -> {
+                if (b) data[i].setSelectedAnswer(3);
+            });
+            option4.setOnCheckedChangeListener((compoundButton, b) -> {
+                if (b) data[i].setSelectedAnswer(4);
+            });
+
+            switch (data[i].getSelectedAnswer()) {
+                case 1:
+                    option1.setChecked(true);
+                    break;
+                case 2:
+                    option2.setChecked(true);
+                    break;
+                case 3:
+                    option3.setChecked(true);
+                    break;
+                case 4:
+                    option4.setChecked(true);
+                    break;
+            }
+
+            return v;
+        }
+    }
+
+}
