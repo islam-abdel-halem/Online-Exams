8b51097e16068f0924f745094f6734d88aff9e3b
ESLAM <102685868+islam-abdel-halem@users.noreply.github.com>
2024-05-15 22:07:38 +0300

    add Questions and Exam page

--- app/src/main/java/com/example/onlineexams/Home.java
+++ app/src/main/java/com/example/onlineexams/Home.java
@@ -1,24 +1,119 @@
 package com.example.onlineexams;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class Home extends AppCompatActivity {
+
+    private String userUID;
+    private String firstName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        ProgressDialog progressDialog = new ProgressDialog(Home.this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        Bundle b = getIntent().getExtras();
+        userUID = Objects.requireNonNull(b).getString("User UID");
+
+        TextView name = findViewById(R.id.name);
+        TextView total_questions = findViewById(R.id.total_questions);
+        TextView total_points = findViewById(R.id.total_points);
+        Button startQuiz = findViewById(R.id.startQuiz);
+        Button createQuiz = findViewById(R.id.createQuiz);
+        RelativeLayout solvedQuizzes = findViewById(R.id.solvedQuizzes);
+        RelativeLayout your_quizzes = findViewById(R.id.your_quizzes);
+        EditText quiz_title = findViewById(R.id.quiz_title);
+        EditText start_quiz_id = findViewById(R.id.start_quiz_id);
+        ImageView signout = findViewById(R.id.signout);
+
+        ValueEventListener listener = new ValueEventListener() {
+            @SuppressLint({"SetTextI18n", "DefaultLocale"})
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                DataSnapshot usersRef = snapshot.child("Users").child(userUID);
+                firstName = Objects.requireNonNull(usersRef.child("First Name").getValue()).toString();
+
+                if (usersRef.hasChild("Total Points")) {
+                    String totalPoints = Objects.requireNonNull(usersRef.child("Total Points").getValue()).toString();
+                    int points = Integer.parseInt(totalPoints);
+                    total_points.setText(String.format("%03d", points));
+                }
+
+                if (usersRef.hasChild("Total Questions")) {
+                    String totalQuestions = Objects.requireNonNull(usersRef.child("Total Questions").getValue()).toString();
+                    int questions = Integer.parseInt(totalQuestions);
+                    total_questions.setText(String.format("%03d", questions));
+                }
+                name.setText("Welcome " + firstName + "!");
+                progressDialog.dismiss();
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(Home.this, "Can't connect", Toast.LENGTH_SHORT).show();
+            }
+        };
+        database.addValueEventListener(listener);
+
+        signout.setOnClickListener(View -> {
+            FirebaseAuth.getInstance().signOut();
+            Intent i = new Intent(Home.this, MainActivity.class);
+            startActivity(i);
+            finish();
         });
+
+        createQuiz.setOnClickListener(v -> {
+            if (quiz_title.getText().toString().equals("")) {
+                quiz_title.setError("Quiz title cannot be empty");
+                return;
+            }
+
+            Intent i = new Intent(Home.this, ExamEditor.class);
+            i.putExtra("Quiz Title", quiz_title.getText().toString());
+            quiz_title.setText("");
+            startActivity(i);
+        });
+
+        startQuiz.setOnClickListener(v -> {
+            if (start_quiz_id.getText().toString().equals("")) {
+                start_quiz_id.setError("Quiz title connot be empty");
+                return;
+            }
+            Intent i = new Intent(Home.this, Exam.class);
+            i.putExtra("Quiz ID", start_quiz_id.getText().toString());
+            start_quiz_id.setText("");
+            startActivity(i);
+        });
+
+
     }
 }
